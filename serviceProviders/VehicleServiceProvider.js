24557bfa60c09e946c04e88066df72df645a3730
Covenant Chukwudi <cchukwudi@tradedepot.co>
2018-10-20 09:37:09 +0100

    completed deleted method for VehicleSeviceProvider

--- serviceProviders/VehicleServiceProvider.js
+++ serviceProviders/VehicleServiceProvider.js
@@ -165,12 +165,58 @@ exports.getVehicleByLicenseNumber = async (licenseNumber) => {
  *
  * @param limit | a number dictating how many should be returned. -1 = everything.
  *
- * @returns {Promise<void>} | resolves to the vehicle object.
+ * @returns {Promise<void>} | resolves to the vehicle array object.
  *
  */
 exports.getAllVehicles = async (limit = -1) => {
-    
+    return (limit > 0 )?
+     await VehicleModel.find({}).limit(limit) : await VehicleModel.find({});
 }
+
+
+/**
+ *
+ * This method is responsible for retrieving all vehicles that exist under a particular list of categories.
+ *
+ * @param categoryNames | the list of the category names.
+ * @param limit | how many should be returned. -1 = everything.
+ *
+ * @returns {Promise<void>} |resolves to the vehicle array object.
+ *
+ */
+exports.getAllVehiclesUnderACategory = async (categoryNames, limit = -1) => {
+    let lowerCaseCategories = [];
+    categoryNames.map((categoryName) => {
+        lowerCaseCategories.push(categoryName.toLocaleLowerCase());
+    });
+
+    return (limit > 0) ? await VehicleModel.find({categories: { $all: lowerCaseCategories }}).limit(limit) :
+        await VehicleModel.find({categories: { $all: lowerCaseCategories }});
+};
+
+/**
+ *
+ * This method is responsible for deleting a vehicle using it's license plate number.
+ *
+ * @param licenseNumber | the license plate number.
+ *
+ * @returns {Object}
+ *
+ */
+exports.deleteVehicleByLicenseNumber = async (licenseNumber) => {
+    licenseNumber = licenseNumber.toLocaleLowerCase();
+
+    //we confirm first that the vehicle exist.
+    let vehicle = await exports.getVehicleByLicenseNumber(licenseNumber);
+    if(!vehicle){
+        return {success: false, data: null, message: 'we couldn\'t find the vehicle in question'};
+    }
+
+    await VehicleModel.findAndRemove({vehicle});
+    return {success: false, data: {}, message: 'vehicle has been deleted.'};
+
+}
+
 
 
 
